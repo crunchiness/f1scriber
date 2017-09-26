4e781fae41b8abe4e670153e397e19d7d46a3068
Ingvaras Merkys <ingvaras@gmail.com>
2017-09-26 23:21:27 +0300

    reading and resampling file with pyav

--- test.py
+++ test.py
@@ -17,17 +17,43 @@ class AudioIterable:
                     raise StopIteration()
 
 
-audio_file_name = 'resources/output.aac'
-container = av.open(audio_file_name)
-for frame in container.decode():
-    print frame
-# n = AudioIterable(audio_file_name, 32*1024)
-# asdf = [len(chunk) for chunk in n]
-# print asdf
+class AudioIterable2:
+    def __init__(self, path):
+        self.container = av.open(path)
+        self.stream = next(s for s in self.container.streams if s.type == 'audio')
 
-# with io.open(audio_file_name, 'rb') as audio_file:
-#     size = -1
-#     while size != 0:
-#         chunk = audio_file.read(32*1024)
-#         size = len(chunk)
-#         print type(chunk), size
+    def __iter__(self):
+        for packet in self.container.demux(self.stream):
+            for frame in packet.decode():
+                yield frame
+        raise StopIteration()
+
+
+audio_file_name = 'resources/output.aac'
+# audio_file_name = 'http://radio.m-1.fm:80/m1/mp3'
+
+ai = AudioIterable2(audio_file_name)
+
+resampler = av.AudioResampler(
+    format=av.AudioFormat('s16').packed,
+    layout='mono',
+    rate=16000,
+)
+
+data = b''
+
+
+def send(smth):
+    print len(smth)
+
+
+for i, frame in enumerate(ai):
+    fram = resampler.resample(frame)
+    data += fram.planes[0].to_bytes()
+    if len(data) > 32*1024:
+        send(data[:32*1024])
+        data = data[32*1024:]
+if len(data) > 0:
+    send(data[:32 * 1024])
+    data = data[32 * 1024:]
+
